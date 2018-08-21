af2af285b99dc35fec36110193d7e1039bf6d4c0
blndev <blndev@users.noreply.github.com>
2018-08-21 23:51:00 +0200

    expose correct port

--- node3CustomExporter/simpleRestExporter.py
+++ node3CustomExporter/simpleRestExporter.py
@@ -20,11 +20,11 @@ CPU_TEMP = Gauge('cpu_temperature', 'Delivers the current temperature of the cpu
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
-    start_http_server(8000)
+    start_http_server(9100)
     # Generate some requests.
     while True:
         time.sleep(30) #run only every 30 seconds
         process_request(random.random())
-        CPU_TEMP.labels(cpu='vCPU-PYthon', core='01').set(random.random()*100)
+        CPU_TEMP.labels(cpu='vCPU-PYthon', core='03').set(random.random()*100)
         CPU_TEMP.labels(cpu='vCPU-PYthon', core='02').set(random.random()*100)
         #get_value(random.random())
